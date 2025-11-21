27ba176a8b5b82c01525b1b9068d9fdbd7297112
KyojoruRengoku <praveensdarc@gmail.com>
2025-11-21 22:24:10 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// let ctr=0;
+// function callback(){
+//     let el=document.querySelectorAll("h4")[1]
+//     el.innerHTML=ctr
+//     ctr=ctr+1;
+// }
+
+
+// setInterval(callback,1000)
+
+function add(){
+    const ele=document.getElementById(add);
+    ele.
+}
+
+
+function deleteTodo(index){
+    const element=document.getElementById("todo-"+index)
+    element.parentNode.removeChild(element)
+}
